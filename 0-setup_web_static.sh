7969ffca5315fb50626364bf0c467df8a9ba4115
Michemor <yatoijema@gmail.com>
2024-07-11 13:13:34 +0300

    Correction of "0. Prepare your web servers"

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,22 +3,14 @@
 
 # checks whether nginx is installed, if not it installs it
 if ! dpkg-query -s nginx &>/dev/null; then
-    sudo apt update
-    sudo apt install nginx -y
-    sudo service nginx start
+    apt update
+    apt install nginx -y
+    service nginx start
 fi
 
 # create the following folders if they don't exist
-folders=("/data/"
-        "/data/web_static/"
-        "/data/web_static/releases/"
-        "/data/web_static/shared/"
-        "/data/web_static/releases/test/")
-
-# iterate through the folders, creating them if it doesn't exist
-for folder in "${folders[@]}"; do
-        sudo mkdir -p "$folder"
-done
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
 
 # create a fake html file
 echo "Holberton School" > /data/web_static/releases/test/index.html
@@ -45,4 +37,4 @@ printf %s "server {
     }
 }" > /etc/nginx/sites-available/default
 
-sudo service nginx restart
+service nginx restart
