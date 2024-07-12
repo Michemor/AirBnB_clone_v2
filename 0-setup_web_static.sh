7cc4549a5153d4858e02794cbf2875b35945b156
jayudoye <joeludoye@gmail.com>
2024-07-11 20:23:04 -0700

    AirBnB clone - Deploy static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
-# sets up web servers for deployment of web_static 
-# checks whether nginx is installed, if not it installs it
+# A Bash script that sets up your web servers for the deployment of web_static
+# Checks if nginx is installed
 sudo apt-get update
 sudo apt-get install nginx -y
 
-# create the following folders if they don't exist
+# Create the following folders
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
 
-# create a fake html file
+# Create a file with HTML content
 sudo echo "<html>
     <head>
     </head>
@@ -17,13 +17,13 @@ sudo echo "<html>
     </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
 
-# check for symbolic link and recreate it
+# Checks for symbolic link
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-# grants ownership to group and user
+# Update file permissions
 sudo chown -R ubuntu:ubuntu /data
 
-# update nginx to serve content for hbnb_static
+# Update nginx to serve content for hbnb_static
 sudo printf %s " server {
     listen 80 default_server;
     listen [::]:80 default_server;
