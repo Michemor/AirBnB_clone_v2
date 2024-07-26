8837fb7f90d2a8ada2a96a9ebf625681ca5973e7
Michemor <yatoijema@gmail.com>
2024-07-26 12:56:49 +0300

    3. Python is cool!

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" This script starts a flask web application """
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """ home page for HBNB website """
+    return ("Hello HBNB!")
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ defines content for hbnb route """
+    return ("HBNB")
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is(text):
+    """ defines a c_is endpoint """
+    output = text.replace("_", " ")
+    return (f"C {output}")
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text="is cool"):
+    """ defines a pytho endpoint """
+    output = text.replace("_", " ")
+    return (f"Python {output}")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
